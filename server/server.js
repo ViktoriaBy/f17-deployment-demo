063cc67079705b29500cd0fa89f0114fe4b6911a
ViktoriaB <v.chamborant@hotmail.com>
2022-02-09 12:12:30 -0600

    fixed

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html')) //path.join joins heroku path(__dirname) with ours
+    res.sendFile(path.join(__dirname, '../public/index.html')) //path.join joins heroku path(__dirname) with ours
 })
 
 
